a0a432e304d7ae9f82a571bb7dea41c0373fed61
Joseph Hajjar <jhhajjar@gmail.com>
2020-06-01 18:48:22 -0400

    downloads all files

--- parse.py
+++ parse.py
@@ -12,7 +12,7 @@ def parse_page(link):
     address = ''
     for i in links.contents:
         if type(i) == bs4.element.Tag:
-            address += i.text    
+            address += i.text + ' ' 
     return address
 
 
@@ -22,22 +22,35 @@ def parse_all(save_to_files=True):
     if save_to_files:
         if not os.path.exists('sotu_speeches'):
             os.makedirs('sotu_speeches')
+    else:
+        speeches = []
+    # Get all links from this page
     link = 'https://www.presidency.ucsb.edu/documents/presidential-documents-archive-guidebook/annual-messages-congress-the-state-the-union'
     correct_links_to_follow = 'https://www.presidency.ucsb.edu/ws/index.php?pid='
     page = requests.get(link).content
     soup = bs4.BeautifulSoup(page, features='html.parser')
     links = soup.find_all('a')
+    # Loop through all links
     for i in links:       
         if str(i.get('href'))[:len(correct_links_to_follow)] == correct_links_to_follow:
-            if len(i.contents[0]) == 4 and int(i.contents[0]) >= 1900:
+            year = re.search('[0-9][0-9][0-9][0-9]', i.contents[0])
+            if year and int(year[0]) >= 1900:
                 try:
-                    f = open(f"sotu_speeches/{i.contents[0]}.txt", "w")
-                    f.write(parse_page(str(i.get('href'))))
-                    f.close()
-                except:
+                    print(f'Getting {i.contents[0]}')
+                    if save_to_files:
+                        f = open(f"sotu_speeches/{i.contents[0]}.txt", "w")
+                        f.write(parse_page(str(i.get('href'))))
+                        f.close()
+                    else:
+                        speeches.append(parse_page(str(i.get('href'))))
+                except Exception as e:
+                    print(e)
                     print(f'Couldnt write {i.contents[0]}. Go find this one manually')
 
+    if not save_to_files:
+        return speeches
+
 # TEST
-print(parse_page('https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-the-state-the-union-27'))
+# print(parse_page('https://www.presidency.ucsb.edu/documents/remarks-the-state-the-union-message-key-west-florida'))
 
 parse_all()
